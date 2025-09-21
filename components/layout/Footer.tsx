2da77f0334608aaad8bece6ee510c30499e4d0a2
letisiaelias-beep <letisiaelias@gmail.com>
2025-09-21 20:40:07 +0300

    Setting up Project-1

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-8 text-center">
+      <p>© 2025 My Project. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
